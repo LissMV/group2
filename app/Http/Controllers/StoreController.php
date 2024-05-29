c55db044a424caa8efde0a80e2115987c6374c59
Echeverria7 <153480776+Echeverria7@users.noreply.github.com>
2024-05-29 10:51:12 -0600

    storeController

---
+++ app/Http/Controllers/StoreController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class StoreController extends Controller
+{
+    public function StoreForm(Request $request) {
+        $credentials = $request->validate([
+            'email' => 'required|email|exists:users,email',
+            'password' => 'required|min:8',
+        ]);
+       }
+
+    public function Register(Request $request) {
+        $credentials = $request->validate([
+            'email'
+        ]);
+    }
+}
+
