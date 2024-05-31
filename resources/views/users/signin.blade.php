02a3c009506b665c94488b33d9266b05316ed2a5
Hazel-Ayala <hazelp617@gmail.com>
2024-05-31 11:00:33 -0600

    commit
    .

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -1,14 +1,15 @@
 <x-layout>
-    <Form class="text-center" action="/Login" method="POST">
+    <Form class="text-center m-8" action="/Login" method="POST">
         @csrf
-        <div>
+        <h1 class="font-bold text-6xl">PERFIL</h1>
+        <div class="font-semibold m-8 text-2xl">
             <div>
-                <x-input name="email" type="text">Email</x-input>
+                <x-input class="bg-slate-500 " name="email" type="text">Email</x-input>
             </div>
             <div>
                 <x-input name="password" type="password">Password</x-input>
             </div>
-            <button type="submit">Ayuda</button>
+            <button class="m-8" type="submit">Ayuda</button>
         </div>
     </Form>
 </x-layout>
