2a0096241929934ea7cc038389407c02c5b7574b
LissMV <valencialisseth47@gmail.com>
2024-06-03 07:43:38 -0600

    flex

--- resources/views/Stores/store_form.blade.php
+++ resources/views/Stores/store_form.blade.php
@@ -4,7 +4,7 @@
         <p class="text-3xl flex justify-center h-14 text-center" syle="font-size: 10px" style="color: #79401E;">PERFIL DE TIENDA</p>
         <Form action="/Register" method="POST">
             @csrf
-
+<div class="flex">
             <div>
                 <img height="300px" width="300px"
                     src="https://cdn.icon-icons.com/icons2/3298/PNG/512/ui_user_profile_avatar_person_icon_208734.png"
@@ -28,5 +28,7 @@
                 </div>
                 <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-red-500"
                     type="submit" >Llenar formulario para venta de productos</button>
+                </div>
+            </div>
         </form>
 </x-layout>
