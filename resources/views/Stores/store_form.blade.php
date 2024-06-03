d966a941c72f56e7dea1aea56ace665e5fb0c350
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-06-02 18:34:05 -0600

    commit

--- resources/views/Stores/store_form.blade.php
+++ resources/views/Stores/store_form.blade.php
@@ -1,25 +1,32 @@
 <x-layout>
-    <div class="flex flex-col justify-center items-center h-screen">
-    <h1 class="text-3xl flex justify-center h-14 text-center  ">PERFIL DE TIENDA</h1>
-    <Form action="/Register" method="POST">
-        @csrf
-    <div>
-        <x-input name="user" type="text">Nombre de tienda</x-input>
-    </div>
-    <div>
-        <x-input name="phone" type="tel">Teléfono</x-input>
-    </div>
-    <div>
-        <x-input name="email" type="email">Email</x-input>
-    </div>
-    <div>
-        <x-input name="name" type="text">Dirección</x-input>
-    </div>
-    <div>
-        <x-input name="name" type="text">Descripción de tienda</x-input>
-    </div>
-    <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-sky-500"
-                    type="submit">Llenar formulario para venta de productos</button>
-    </form>
-</x-layout>
 
+    <div class="flex flex-col justify-center items-center h-screen">
+        <p class="text-3xl flex justify-center h-14 text-center" syle="font-size: 10px" style="color: #79401E;">PERFIL DE TIENDA</p>
+        <Form action="/Register" method="POST">
+            @csrf
+
+            <div>
+                <img height="300px" width="300px"
+                    src="https://cdn.icon-icons.com/icons2/3298/PNG/512/ui_user_profile_avatar_person_icon_208734.png"
+                    alt="">
+            </div>
+            <div>
+                <div class="h-14">
+                    <x-input name="user" type="text" style="background: #BDBBBA " class="border-neutral-950 border solid-xl px-4 py-1.5">Nombre de tienda</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="phone" type="tel" style="background: #BDBBBA " class="border-double border-neutral-950 border solid-xl px-4 py-1.5">Teléfono</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="email" type="email" style="background: #BDBBBA " class="border-double border-neutral-950 border solid-xl px-4 py-1.5">Email</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="name" type="text" style="background: #BDBBBA " class="border-neutral-950 border solid-xl px-4 py-1.5">Dirección</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="name" type="text" style="background: #BDBBBA " class="border-neutral-950 border solid-xl px-4 py-1.5">Descripción de tienda</x-input>
+                </div>
+                <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-red-500"
+                    type="submit" >Llenar formulario para venta de productos</button>
+        </form>
+</x-layout>
