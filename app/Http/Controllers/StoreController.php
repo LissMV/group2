1880f229b2c49c41f0b7468625de93399878cb16
Echeverria7 <153480776+Echeverria7@users.noreply.github.com>
2024-06-03 10:20:13 -0600

    storeController

--- app/Http/Controllers/StoreController.php
+++ app/Http/Controllers/StoreController.php
@@ -6,12 +6,32 @@ use Illuminate\Http\Request;
 
 class StoreController extends Controller
 {
-    public function StoreForm(Request $request) {
-        $credentials = $request->validate([
+    public function index() {
+
+    }
+
+    public function create() {
+
+    }
+
+    public function store() {
+        $store = $request->validate([
             'email' => 'required|email|exists:users,email',
             'password' => 'required|min:8',
         ]);
-       }
+    }
+
+    public function edit() {
+
+    }
+
+    public function update() {
+
+    }
+
+    public function destroy() {
+
+    }
 
     public function Register(Request $request) {
         $credentials = $request->validate([
