975633196788385a1fb0443d9efa25d2ebdd92c7
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-06-05 07:43:40 -0600

    commit

--- resources/views/products/product_form.blade.php
+++ resources/views/products/product_form.blade.php
@@ -1,26 +1,40 @@
 <x-layout>
-    <Form action="/Register" method="POST">
-        @csrf
-    <div>
-        <x-input name="price" type="text">Disponible</x-input>
-    </div>
-    <div>
-        <x-input name="email" type="text">Precio</x-input>
-    </div>
-    <div>
-        <x-input name="password" type="text">Descuento</x-input>
-    </div>
-    <div>
-        <x-input name="password" type="text">Nombre de producto</x-input>
 
-    </div>
-    <div>
-        <x-input name="password" type="text">Subtitulo</x-input>
-    </div>
-    <div>
-        <x-input name="password" type="password">Descripción</x-input>
-        <label for=""></label>
-    </div>
-    </form>
+    <div class="flex flex-col justify-center items-center h-screen">
+        <p class="text-3xl flex justify-center h-14 text-center" syle="font-size: 10px" style="color: #79401E;">AGREGAR PRODUCTOS</p>
+        <Form action="/Register" method="POST">
+            @csrf
+<div class="flex">
+            <div>
+                <img height="300px" width="300px"
+                    src="https://img.freepik.com/vector-gratis/ilustracion-papel_53876-5846.jpg?size=626&ext=jpg&ga=GA1.1.1788068356.1716508800&semt=ais_user"
+                    alt="">
+            </div>
+            <div>
+                <div class="h-14">
+                    <x-input name="user" type="text" >Esta disponible</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="phone" type="tel" >Precio</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="email" type="email">Descuento</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="name" type="text" >Nombre</x-input>
+                </div>
+                <div class="h-14">
+                    <x-input name="name" type="text">Subtitulo</x-input>
+                </div>
+                <div>
+                    <div class="h-14">
+                        <x-input name="name" type="text">Descripción</x-input>
+                    </div>
+                </div>
+                <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-red-500"
+                    type="submit" >Subir producto</button>
+                </div>
+            </div>
+        </form>
 </x-layout>
 
