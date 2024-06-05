d7de8d79390d6c70185e77e152df42b7530a5274
Hazel-Ayala <hazelp617@gmail.com>
2024-06-05 01:23:05 -0600

    commit

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -1,15 +1,17 @@
 <x-layout>
-    <Form class="text-center m-8" action="/Login" method="POST">
+    <Form class="text-center my-16 " action="/Login" method="POST">
         @csrf
-        <h1 class="font-bold text-6xl">PERFIL</h1>
+        <h1 class="font-bold text-6xl">REGISTRARSE</h1>
         <div class="font-semibold m-8 text-2xl">
-            <div>
-                <x-input class="bg-slate-500 " name="email" type="text">Email</x-input>
+            <div class=" m-8">
+                <x-input name="email" type="text">Email</x-input>
             </div>
             <div>
                 <x-input name="password" type="password">Password</x-input>
             </div>
-            <button class="m-8" type="submit">Ayuda</button>
+            <button class=" m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-indigo-500 "
+                type="submit">Ayuda</button>
         </div>
     </Form>
+
 </x-layout>
--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -1,17 +1,22 @@
 <x-layout>
-    <Form action="/Register" method="POST">
+
+    <Form class="text-center my-16" action="/Register" method="POST">
         @csrf
         <div>
-            <div>
-                <x-input name="user" type="text">User name</x-input>
+            <h1 class="font-bold text-6xl">REGISTRARSE</h1>
+            <div class="font-semibold m-8 text-2xl">
+                <div class="m-8">
+                    <x-input name="user" type="text">User name</x-input>
+                </div>
+                <div class="m-8">
+                    <x-input name="email" type="text">Email</x-input>
+                </div>
+                <div class="m-8">
+                    <x-input name="password" type="password">Password</x-input>
+                </div>
+                <button class="m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-indigo-500 "
+                    type="submit">Crear cuenta</button>
             </div>
-            <div>
-                <x-input name="email" type="text">Email</x-input>
-            </div>
-            <div>
-                <x-input name="password" type="password">Password</x-input>
-            </div>
-            <button type="submit">Ayuda x2</button>
         </div>
     </Form>
 </x-layout>
