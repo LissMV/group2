e1e9ff2ed891e7ec21648a488a2c6625988b1a1c
LissMV <valencialisseth47@gmail.com>
2024-06-06 07:26:30 -0600

    Update store.blade.php

--- resources/views/Stores/store.blade.php
+++ resources/views/Stores/store.blade.php
@@ -1,3 +1,31 @@
-<div>
-    <!-- He who is contented is rich. - Laozi -->
-</div>
+<x-layout>
+ <Form class="text-center my-16" action="/create" method="POST">
+    @csrf
+    <div>
+        <h1 class="font-bold text-4xl m-8">Formulario para solicitar acceso para vender</h1>
+    </div>
+    <div>
+        <div class="h-14">
+            <x-input name="user" type="text" >Nombre de tienda</x-input>
+        </div>
+        <div class="h-14">
+            <x-input name="phone" type="tel" >Teléfono</x-input>
+        </div>
+        <div class="h-14">
+            <x-input name="email" type="email">Email</x-input>
+        </div>
+        <div class="h-14">
+            <x-input name="name" type="text" >Dirección</x-input>
+        </div>
+        <div class="h-14">
+            <x-input name="name" type="text">Descripción de tienda</x-input>
+        </div>
+        <div class="h-14">
+            <x-input name="name" type="text">Redes sociales</x-input>
+        </div>
+
+        <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-red-500"
+        type="submit" >Ayudaa</button>
+    </div>
+ </form>
+</x-layout>
