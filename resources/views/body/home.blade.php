d5cf65894f44b805ac316a94e2cce06a86aec684
Hazel-Ayala <hazelp617@gmail.com>
2024-06-12 01:00:38 -0600

    home :)
    .

--- resources/views/body/home.blade.php
+++ resources/views/body/home.blade.php
@@ -1,3 +1,93 @@
-<div>
-    <h1>Welcome</h1>
-</div>
+<x-layout>
+
+    <x-input name="busqueda" type="text"></x-input>
+
+    <img height="600px" width="800px" src="{{ asset('img/se diferente.png') }}" alt="">
+
+    <button class=" m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-indigo-500 "
+        type="submit">Comunidad</button>
+
+    <button class=" m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-indigo-500 "
+        type="submit">Aprende más</button>
+
+    <div>
+            <div>
+                <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2>
+
+        </div>
+
+            <div>
+                <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2>
+
+        </div>
+
+            <div>
+            <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2> 
+
+        </div>
+
+            <div>
+                <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2>
+
+        </div>
+
+            <div>
+                <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2>
+
+        </div>
+
+            <div>
+                <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+            </div>
+            <div>
+                <p>Product</p>
+                <h2>$10.99</h2>
+
+            </div>
+    </div>
+
+    <div>
+        <h1 class=" my-8 text-2xl">Heading</h1>
+
+    </div>
+
+    <div>
+
+        <div>
+            <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+        </div>
+
+        <div>
+            <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+        </div>
+
+        <div>
+            <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+        </div>
+
+        <div>
+            <img height="200px" width="400px" src="{{ asset('img/se diferente.png') }}" alt="">
+        </div>
+    </div>
+
+</x-layout>
