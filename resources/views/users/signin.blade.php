46087c7ec4eff20a5dd71c25f1f59c1fac4b7d80
Echeverria7 <153480776+Echeverria7@users.noreply.github.com>
2024-06-12 10:56:29 -0600

    login

--- resources/views/users/signin.blade.php
+++ resources/views/users/signin.blade.php
@@ -1,18 +1,25 @@
 <x-layout>
-<div>
-    <h1 class="font-bold text-4xl m-8 text-center">Log in</h1>
-    <form class="flex justify-center items-center " action="/login" method="POST">
-        @csrf
-        <div class="w-96 p-6">
-        <div>
-           <x-input name="email" type="text">Email</x-input>
-        </div>
-        <div>
-            <x-input name="password" type="password">Password</x-input>
-        </div>
-        <x-button>Log in</x-button>
+    <div class="grid grid-cols-3 h-screen place-content-center ">
+        <div class="col-span-1">
+
         </div>
 
-    </form>
-</div>
+        <div class="col-span-2">
+            <h1 class="font-bold text-4xl m-8 text-center">Log in</h1>
+            <form class="flex justify-center items-center " action="/login" method="POST">
+                @csrf
+                <div class="w-96 p-6">
+                    <div>
+                        <x-input name="email" type="text">Email</x-input>
+                    </div>
+                    <div>
+                        <x-input name="password" type="password">Password</x-input>
+                    </div>
+                   
+                    <x-button>Log in</x-button>
+                </div>
+
+            </form>
+        </div>
+    </div>
 </x-layout>
