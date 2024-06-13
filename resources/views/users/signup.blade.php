5ef0dd7d087b04719f0b68f77eca11744b669c2e
Echeverria7 <153480776+Echeverria7@users.noreply.github.com>
2024-06-13 00:38:43 -0600

    register bonito

--- resources/views/users/signup.blade.php
+++ resources/views/users/signup.blade.php
@@ -1,21 +1,27 @@
 <x-layout>
-    <div>
-        <h1 class="font-bold text-4xl m-8 text-center ">Sign Up</h1>
-    <form class="flex justify-center items-center" action="/register" method="POST">
-        @csrf
-
-        <div class="w-96 p-6">
-            <div>
-                <x-input name="name" type="text">Name</x-input>
-            </div>
-            <div>
-                <x-input name="email" type="text">Email</x-input>
-            </div>
-            <div>
-                <x-input name="password" type="password">Password</x-input>
-            </div>
-            <x-button>Register</x-button>
+    <div class="grid grid-cols-3 h-screen place-content-center ">
+        <div class="col-span-1">
+            <img class="h-screen" src="{{ asset('img/forms/biobio.png') }}" alt="">
         </div>
-    </form>
+
+        <div class="col-span-2 m-24">
+            <h1 class="font-bold text-4xl m-8 text-center pb 8">Sign Up</h1>
+            <form class="flex justify-center items-center" action="/register" method="POST">
+                @csrf
+
+                <div class="w-96 p-6">
+                    <div pb-8>
+                        <x-input name="name" type="text">Name</x-input>
+                    </div pb-8>
+                    <div>
+                        <x-input name="email" type="text">Email</x-input>
+                    </div>
+                    <div pb-8>
+                        <x-input name="password" type="password">Password</x-input>
+                    </div pb-8>
+                    <x-button>Register</x-button>
+                </div>
+            </form>
+        </div>
     </div>
 </x-layout>
