374acd960f5727393d61c89cfc8ef48f268c52a7
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-06-13 09:18:21 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,18 @@
 <x-layout>
-    <div class="flex flex-col justify-center items-center h-screen" >
+    <div class="flex flex-col justify-center items-center h-screen">
         <div class="search-container">
             <input type="text" placeholder="Buscar...">
-            <button type="submit"><i class="fas fa-search"></i></button>
+            <button type="button" class="cursor-pointer ...">
+                Submit
+            </button>
         </div>
+
         <div>
             <img height="600px" width="800px" src="{{ asset('img/se diferente.png') }}" alt="">
+        </div class="h-14">
+        <button class=" border-double border-neutral-950 border rounded-xl px-4 py-1.5"
+            type="submit">Registrarse</button>
+        <button type="submit">Iniciar sesión</button>
         </div>
     </div>
 </x-layout>
