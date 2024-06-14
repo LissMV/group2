7a22438a983e7d18652f8b62f331a0c3fdb4ee9b
KarlyCardona07 <153212043+KarlyCardona07@users.noreply.github.com>
2024-06-14 11:37:08 -0600

    See products
    .

--- resources/views/products/see_products.blade.php
+++ resources/views/products/see_products.blade.php
@@ -1,3 +1,141 @@
-<div>
-    <h1>holi Karly</h1>
-</div>
+<x-layout>
+    
+    <header class="flex justify-between items-center bg-[#829540] p-6 text-white">
+        <a href="#" class="flex items-center text-lg font-semibold">
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 mr-2" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 19l-7-7 7-7" />
+            </svg>
+            Ecommunity
+        </a>
+        <a href="#" class="flex items-center text-lg font-semibold">
+            
+            <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 24 24" stroke="currentColor">
+                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-1 5h13m-1-5l1-5m-5 10a1 1 0 110 2 1 1 0 010-2zm-6 0a1 1 0 110 2 1 1 0 010-2z" />
+                <circle cx="8.5" cy="21" r="2" />
+                <circle cx="16.5" cy="21" r="2" />
+            </svg>
+        </a>
+    </header>
+
+    <!-- Contenido principal -->
+    <div class="max-w-screen-xl mx-auto px-4 py-8">
+        <div class="flex items-center space-x-8">
+
+            <!-- Imagen principal -->
+            <div class="w-1/2">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQrSy57tnkq38zr5EvxJUsFOy5mt7kFD545iZCVOdI9a3uQ5ZO9" alt="Imagen de producto" class="w-full max-w-full rounded-lg shadow-md">
+            </div>
+            
+            <!-- Información de producto -->
+            <div class="w-1/2">
+                <div class="text-left">
+                    <p class="text-2xl font-bold mb-4">Cepillos dentales</p>
+                    <div class="text-lg text-gray-600">
+                        <p>Cepillos de bambú</p>
+                        <p>$4.25</p>
+                        <p class="text-sm font-bold">Body text for describing why this product is simply a must-buy</p>
+                    </div>
+                    <button class="font-bold  text-1xl m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-light-brown "
+        type="submit">Comunidad</button>
+                </div>
+            </div>
+        </div>
+
+        <!-- Sección: Más productos-->
+        <div class="mb-8">
+            <h2 class="text-2xl font-bold mb-4">Más productos</h2>
+            <div class="flex items-center space-x-4">
+                <!-- Imágenes de más productos -->
+                <div>
+                    <img src="https://ecocash.es/9655-large_default/bastoncillos-de-algodon-de-bambu-caja-200uds.jpg" alt="Producto 1" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+                <div>
+                    <img src="https://m.media-amazon.com/images/I/81BvvybEzuL.__AC_SX300_SY300_QL70_ML2_.jpg" alt="Producto 2" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+                <div>
+                    <img src="https://i.pinimg.com/564x/a2/80/88/a28088598903a6d3786a29af03d08da9.jpg" alt="Producto 3" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+            </div>
+        </div>
+    </div>
+
+        <!-- Contenido principal -->
+        <div class="max-w-screen-xl mx-auto px-4 py-8">
+        <div class="flex items-center space-x-8">
+            <!-- Imagen principal -->
+            <div class="w-1/2">
+                <img src="https://i.pinimg.com/564x/e4/f6/60/e4f6601561d93f460c2734b22685b556.jpg" alt="Imagen de producto" class="w-full max-w-full rounded-lg shadow-md">
+            </div>
+            
+            <!-- Información de producto -->
+            <div class="w-1/2">
+                <div class="text-left">
+                    <p class="text-2xl font-bold mb-4">Coco Rose</p>
+                    <div class="text-lg text-gray-600">
+                        <p>Exfoliante</p>
+                        <p>$8.50</p>
+                        <p class="text-sm font-bold">Body text for describing why this product is simply a must-buy</p>
+                    </div>
+                    <button class="font-bold  text-1xl m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-light-brown "
+        type="submit">Comunidad</button>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- Sección: Más productos -->
+        <div class="mb-8">
+            <h2 class="text-2xl font-bold mb-4">Más productos</h2>
+            <div class="flex items-center space-x-4">
+                <!-- Imágenes de más productos -->
+                <div>
+                    <img src="https://i.pinimg.com/564x/9f/22/49/9f22496f3104b445e125570f89202621.jpg" alt="Producto 1" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Contenido principal -->
+    <div class="max-w-screen-xl mx-auto px-4 py-8">
+        <div class="flex items-center space-x-8">
+            <!-- Imagen principal -->
+            <div class="w-1/2">
+                <img src="https://i.pinimg.com/564x/e6/47/cd/e647cd609d70e3846c3828a70ec09eb7.jpg" alt="Imagen de producto" class="w-full max-w-full rounded-lg shadow-md">
+            </div>
+            
+            <!-- Información de producto -->
+            <div class="w-1/2">
+                <div class="text-left">
+                    <p class="text-2xl font-bold mb-4">Paleta de maquillaje</p>
+                    <div class="text-lg text-gray-600">
+                        <p>Maquillaje</p>
+                        <p>$3.50</p>
+                        <p class="text-sm font-bold">Body text for describing why this product is simply a must-buy</p>
+                    </div>
+                    <button class="font-bold  text-1xl m-8 border-double border-neutral-950 border rounded-xl px-4 py-1.5  hover:bg-light-brown "
+        type="submit">Comunidad</button>
+                </div>
+            </div>
+        </div>
+        
+
+        <!-- Sección: Más productos -->
+        <div class="mb-8">
+            <h2 class="text-2xl font-bold mb-4">Más productos</h2>
+            <div class="flex items-center space-x-4">
+
+                <!-- Imágenes de más productos -->
+                <div>
+                    <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTAcoX5-iLXT8fAxOqy5rZbu6KkiQToVRvlvUparKUsQZCPsYUR" alt="Producto 1" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+                <div>
+                    <img src="https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcTqxeMb0eHkpNYXh2AAPeR3TTccV_2ZyMFy7RsMtXkYWIY261nI" alt="Producto 2" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+                <div>
+                    <img src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQilz_EDpXSqNKbFzAJzmUofPAfMq4DMEBhQiGqnaIF_aNF59Y9" alt="Producto 3" class="w-32 h-auto rounded-lg shadow-md">
+                </div>
+            </div>
+        </div>
+</x-layout>
+
+
