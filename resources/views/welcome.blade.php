9dffba204612c0798ca0686c9f84288b1e06283e
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-06-19 21:34:17 -0600

    welcome almost "complete"

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,11 +1,11 @@
 <x-layout>
     <section class="text-center">
-        <div class="flex justify-center mb-6 ">
+        <div class="flex justify-center mb-6">
             <section>
         <img height="50px" width="50px" src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png" alt="">
         </button>
         </section>
-            <input class=" bg-light-brown w-4/5 h-7 placeholder-white text-2xl font-serif " type="text" placeholder="Buscar">
+            <input class=" bg-light-brown w-4/5 h-10 placeholder-white text-2xl font-serif " type="text" placeholder="Buscar">
             <section>
 
     </button>
@@ -19,32 +19,33 @@
     <section class="grid grid-cols-2 content-center">
 
         <div class="ml-[250px]">
-            <img height="300px" width="300px"
+            <img height="3500px" width="350px"
                 src="https://i.pinimg.com/564x/d4/43/49/d4434992446bec96ddecb79f266b346c.jpg"
                 alt="">
         </div>
         <div>
-            <img height="300px" width="300px"
+            <img height="350px" width="350px"
                 src="https://acdn.mitiendanube.com/stores/001/129/542/products/cotonetes-meraki1-d353325a4ecb0c8ee116305266209283-1024-1024.jpg"
                 alt="">
-        </div>
+
+            </div>
         <div class="ml-[250px]">
-            <img height="300px" width="300px"
+            <img height="4000px" width="350px"
                 src="https://www.cleanipedia.com/images/iohqr4whhl17/AaXnlgmOnQkBB5t6PrRXa/139eebae1f5ecb3db35c373661bc3df7/SG94YnlfQ2xlYW5pcGVkaWFfXzMyOV8uanBn/1200w/c%C3%B3mo-usar-los-productos-de-limpieza-ecol%C3%B3gicos-correctamente.jpg"
                 alt="">
         </div>
         <div>
-            <img height="300px" width="300px"
+            <img height="250px" width="250px"
                 src="https://www.trebolverde.es/wp-content/uploads/2021/11/limpiadormaderayparquet.jpg"
                 alt="">
         </div>
         <div class="ml-[250px]">
-            <img height="300px" width="300px"
+            <img height="350px" width="350px"
                 src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTn3kDieGTKKkOZ87WCFEm9-79-vgSfegSxrQ&s"
                 alt="">
         </div>
         <div>
-            <img height="300px" width="300px"
+            <img height="350px" width="350px"
                 src="https://i.etsystatic.com/31443243/r/il/989c0f/3906202515/il_570xN.3906202515_alu2.jpg"
                 alt="">
         </div>
@@ -52,12 +53,12 @@
     </section>
 
         <div class="text-center">
-        <button class="border-double border-neutral-950 border rounded-xl px-4 py-1.5   type= text color: white color-background: bg-light-green style=;"
+        <button class="border-double border-neutral-950 border rounded-xl px-20 py-5 color-background: bg-light-green"
             type="submit">Registrarse</button>
             <div>
 
             </div>
-        <button type="submit">Iniciar sesión</button>
+        <button class="px-20 py-5 type=submit">Iniciar sesión</button>
         </div>
 
 </x-layout>
