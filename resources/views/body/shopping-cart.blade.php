eba3bb1bf7a3db2752542aec75db166860e7817c
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-07-05 11:31:52 -0600

    shopping cart

--- resources/views/body/shopping-cart.blade.php
+++ resources/views/body/shopping-cart.blade.php
@@ -1,3 +1,78 @@
-<div>
-    <!-- An unexamined life is not worth living. - Socrates -->
-</div>
+<x-layout>
+                <strong>Carrito de compras</strong>
+<hr class="my-10">
+            </div>
+            <table class="cart-table">
+                <thead>
+                    <tr>
+                        <th>Detalles de productos</th>
+                        <th>Cantidad</th>
+                        <th>Precio</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>
+                            <div class="product-details">
+                                <img src="https://www.sephora.com/productimages/sku/s1754548-av-8-zoom.jpg?imwidth=315" alt="Coco Rose" class="product-image height=">
+                                <div>
+                                    <strong>Coco Rose</strong><br>
+                                    Exfoliante<br>
+                                    Remover
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity-controls">
+                                <button>-</button>
+                                <input type="text" value="1">
+                                <button>+</button>
+                            </div>
+                        </td>
+                        <td>$8.50</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="product-details">
+                                <img src="https://www.idun-nature.com/images/products/zao-make-up-paleta-sombras-clin-l-oeil-n2.jpg" alt="Product 2" class="product-image 300px" width="350px">
+                                <div>
+                                    <strong>Maquillaje</strong><br>
+                                    Descripción<br>
+                                    Remover
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity-controls">
+                                <button>-</button>
+                                <input type="text" value="1">
+                                <button>+</button>
+                            </div>
+                        </td>
+                        <td>$8.50</td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <div class="product-details">
+                                <img src="https://d22fxaf9t8d39k.cloudfront.net/47bcf53c18bcfd72061bcf3322313e118738ae0750b0bd24490e6f4283e35b5816928.jpeg" alt="Product 3" class="product-image 300px" width="350px">
+                                <div>
+                                    <strong>Cepillos dentales</strong><br>
+                                    Descripción<br>
+                                    Remover
+                                </div>
+                            </div>
+                        </td>
+                        <td>
+                            <div class="quantity-controls">
+                                <button>-</button>
+                                <input type="text" value="1">
+                                <button>+</button>
+                            </div>
+                        </td>
+                        <td>$8.50</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    <section>
+</x-layout>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,73 +1,35 @@
 <x-layout>
-    <section class="text-center h-20">
-        <div class="flex justify-center mb-6 h-10">
-
-            <img height="50px" width="50px"
-                src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Adidas_Logo.svg/2560px-Adidas_Logo.svg.png"
-                alt="">
+<body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
+    <div style="background-color: #606c38; padding: 10px 20px; display: flex; justify-content: space-between; align-items: center; color: white;">
 
             <input
-                class=" bg-light-brown w-3/5 h-10 border-double border-neutral-950 border rounded-xl placeholder-white text-1.7xl font-serif"
+                class=" bg-white w-3/5 h-10 border-double border-neutral-950 border rounded-xl placeholder-black text-1.7xl font-serif"
                 type="text" placeholder="Buscar">
 
-
             </button>
-            <button type="submit"><img height="50px" width="50px"
-                    src="https://static.vecteezy.com/system/resources/previews/009/589/567/non_2x/magnifying-icon-magnifying-clipart-transparent-free-png.png"
-                    alt=""></button>
+            <button type="submit"><img height="25px" width="25px"
+                    src="https://cdn-icons-png.flaticon.com/256/25/25313.png"
             </form>
-        </div>
-    </section>
-    <section>
-    <div class="flex justify-center">
-        <img height="500px" width="600px" src="https://www.ecowatch.com/wp-content/uploads/2021/09/176196750-img.jpg"
-            alt="">
-    </div>
-    <section>
-        <p class="ml-[250px]  text-brown  font-serif text-2xl m-8">Productos que encontrarás:</p>
-    </section>
-</section>
-    <section class="grid grid-cols-2 content-center">
-        <div>
-            <img class="ml-[250px] height=300px" width="350px" src="{{ asset('img/forms/welcome.png') }}" alt="">
-        </div>
-        <div>
-            <img height="400px" width="350px"
-                src="https://inlovemag.es/wp-content/uploads/2020/02/recetas-de-limpieza-caseras-800x500.jpg"
-                alt="">
-        </div>
-        <div>
-            <img class="ml-[250px] height=450px" width="370px"
-                src="https://blog.homedepot.com.mx/wp-content/uploads/2021/01/Co%CC%81mo-germinar-semillas-en-macetas-biodegradables.jpg" alt="">
-        </div>
-        <div>
-            <img height="300px" width="350px"
-                src="https://acdn.mitiendanube.com/stores/001/129/542/products/cotonetes-meraki1-d353325a4ecb0c8ee116305266209283-1024-1024.jpg"
-                alt="">
-        </div>
-        <div>
-            <img class="ml-[250px] height=350px" width="350px"
-                src="https://dcdn.mitiendanube.com/stores/001/942/058/products/dientes1-a7baf962af00febdca16734606296216-240-0.png"
-                alt="">
-        </div>
-        <div>
-            <img height="350px" width="350px"
-                src="https://m.media-amazon.com/images/I/71MOlm7WtDL._AC_UF350,350_QL80_.jpg"
-                alt="">
-        </div>
-        <div>
-            <img class="ml-[300px] height=700px" width="900px" src="{{ asset('img/forms/welcome n2.png') }}" alt="">
-        </div>
-    </section>
 
-    <div class="text-center">
-        <button
-            class="border-double border-neutral-950 border rounded-xl px-20 py-3  bg-light-green  text-white font-serif"
-            type="submit">Registrarse</button>
-        <div>
 
-        </div>
-        <button class="px-20 py-5 type=submit font-serif">Iniciar sesión</button>
     </div>
+    <div style="display: flex; flex-direction: column; align-items: center; padding: 20px;">
+        <img src="header-image.png" alt="Header Image" style="width: 80%; margin-bottom: 20px;">
+        <div style="display: flex; margin: 20px 0;">
+            <div style="flex: 1; margin: 0 10px;">
+
+            </div>
+            <div style="flex: 1; margin: 0 10px;">
+
+            </div>
+        </div>
+        <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
+            <button style="width: 150px; padding: 10px; margin: 10px 0; border: none; cursor: pointer; color: white; background-color: #606c38;">Registrarse</button>
+            <button style="width: 150px; padding: 10px; margin: 10px 0; border: none; cursor: pointer; color: black; background-color: white;">Iniciar Sesión</button>
+        </div>
+    </div>
+</body>
 
 </x-layout>
+
+
