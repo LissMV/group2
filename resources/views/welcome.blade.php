960fabfb0248dfba2f263a8fda87c9ebe541d7fc
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-07-12 10:06:35 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,13 +20,13 @@
 
             <img height="100px" width="300px"src="https://blog.homedepot.com.mx/wp-content/uploads/2021/01/Co%CC%81mo-germinar-semillas-en-macetas-biodegradables.jpg" alt="">
 
-            <img height="150px" width="300px" src="https://integrishealth.org/-/media/blog/19-jan-feb/jars-with-skincare-products.ashx?revision=007004b7-92c9-4ffa-b02f-47f85cf25894" alt="">
+            <img height="200px" width="350px" src="https://integrishealth.org/-/media/blog/19-jan-feb/jars-with-skincare-products.ashx?revision=007004b7-92c9-4ffa-b02f-47f85cf25894" alt="">
 
             <img height="100px" width="300px" src="https://m.media-amazon.com/images/I/71MOlm7WtDL._AC_UF350,350_QL80_.jpg" alt="">
 
             <img height="100px" width="300px" src="https://ae01.alicdn.com/kf/S4c6d6e6c7a414c8aad3b1c193372358bH.jpg?width=1000&height=1000&hash=2000" alt="">
 
-            <img height="100px" width="350px" src="https://dcdn.mitiendanube.com/stores/001/942/058/products/dientes1-a7baf962af00febdca16734606296216-240-0.png" alt="">
+            <img height="100px" width="300px" src="https://dcdn.mitiendanube.com/stores/001/942/058/products/dientes1-a7baf962af00febdca16734606296216-240-0.png" alt="">
         </section>
         <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
             <button style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: white; background-color: #606c38;">Registrarse</button>
