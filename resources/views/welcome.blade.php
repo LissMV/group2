2c25b1a03695afbd0450fb046544fd726d33514f
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-07-12 10:01:36 -0600

    welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,35 +1,38 @@
 <x-layout>
-<body style="font-family: Arial, sans-serif; margin: 0; padding: 0;">
     <div style="background-color: #606c38; padding: 10px 20px; display: flex; justify-content: space-between; align-items: center; color: white;">
 
             <input
                 class=" bg-white w-3/5 h-10 border-double border-neutral-950 border rounded-xl placeholder-black text-1.7xl font-serif"
                 type="text" placeholder="Buscar">
 
-            </button>
+
             <button type="submit"><img height="25px" width="25px"
-                    src="https://cdn-icons-png.flaticon.com/256/25/25313.png"
-            </form>
+                    src="https://cdn-icons-png.flaticon.com/256/25/25313.png">
+                </button>
 
 
     </div>
-    <div style="display: flex; flex-direction: column; align-items: center; padding: 20px;">
-        <img src="header-image.png" alt="Header Image" style="width: 80%; margin-bottom: 20px;">
-        <div style="display: flex; margin: 20px 0;">
-            <div style="flex: 1; margin: 0 10px;">
+    <div style="display: flex; flex-direction: column; align-items: center">
+        <img src="https://www.ecowatch.com/wp-content/uploads/2021/09/176196750-img.jpg"  style="width: 500px;">
 
-            </div>
-            <div style="flex: 1; margin: 0 10px;">
+        <section class="grid grid-cols-3 content-center">
+            <img height="100px" width="350px" src="https://inlovemag.es/wp-content/uploads/2020/02/recetas-de-limpieza-caseras-800x500.jpg" alt="">
 
-            </div>
-        </div>
+            <img height="100px" width="300px"src="https://blog.homedepot.com.mx/wp-content/uploads/2021/01/Co%CC%81mo-germinar-semillas-en-macetas-biodegradables.jpg" alt="">
+
+            <img height="150px" width="300px" src="https://integrishealth.org/-/media/blog/19-jan-feb/jars-with-skincare-products.ashx?revision=007004b7-92c9-4ffa-b02f-47f85cf25894" alt="">
+
+            <img height="100px" width="300px" src="https://m.media-amazon.com/images/I/71MOlm7WtDL._AC_UF350,350_QL80_.jpg" alt="">
+
+            <img height="100px" width="300px" src="https://ae01.alicdn.com/kf/S4c6d6e6c7a414c8aad3b1c193372358bH.jpg?width=1000&height=1000&hash=2000" alt="">
+
+            <img height="100px" width="350px" src="https://dcdn.mitiendanube.com/stores/001/942/058/products/dientes1-a7baf962af00febdca16734606296216-240-0.png" alt="">
+        </section>
         <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
-            <button style="width: 150px; padding: 10px; margin: 10px 0; border: none; cursor: pointer; color: white; background-color: #606c38;">Registrarse</button>
-            <button style="width: 150px; padding: 10px; margin: 10px 0; border: none; cursor: pointer; color: black; background-color: white;">Iniciar Sesión</button>
+            <button style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: white; background-color: #606c38;">Registrarse</button>
+            <button style="width: 150px;  margin: 10px 0;  cursor: pointer; color: black; background-color: white;">Iniciar Sesión</button>
         </div>
     </div>
-</body>
-
 </x-layout>
 
 
