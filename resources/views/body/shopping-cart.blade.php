fa1719566e8e95d097d6019771e8f918bca1a612
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-07-21 17:19:39 -0600

    calendar

--- resources/views/body/shopping-cart.blade.php
+++ resources/views/body/shopping-cart.blade.php
@@ -1,78 +1,108 @@
 <x-layout>
-                <strong>Carrito de compras</strong>
-<hr class="my-10">
-            </div>
-            <table class="cart-table">
-                <thead>
-                    <tr>
-                        <th>Detalles de productos</th>
-                        <th>Cantidad</th>
-                        <th>Precio</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>
-                            <div class="product-details">
-                                <img src="https://www.sephora.com/productimages/sku/s1754548-av-8-zoom.jpg?imwidth=315" alt="Coco Rose" class="product-image height=">
-                                <div>
-                                    <strong>Coco Rose</strong><br>
-                                    Exfoliante<br>
-                                    Remover
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="quantity-controls">
-                                <button>-</button>
-                                <input type="text" value="1">
-                                <button>+</button>
-                            </div>
-                        </td>
-                        <td>$8.50</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="product-details">
-                                <img src="https://www.idun-nature.com/images/products/zao-make-up-paleta-sombras-clin-l-oeil-n2.jpg" alt="Product 2" class="product-image 300px" width="350px">
-                                <div>
-                                    <strong>Maquillaje</strong><br>
-                                    Descripción<br>
-                                    Remover
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="quantity-controls">
-                                <button>-</button>
-                                <input type="text" value="1">
-                                <button>+</button>
-                            </div>
-                        </td>
-                        <td>$8.50</td>
-                    </tr>
-                    <tr>
-                        <td>
-                            <div class="product-details">
-                                <img src="https://d22fxaf9t8d39k.cloudfront.net/47bcf53c18bcfd72061bcf3322313e118738ae0750b0bd24490e6f4283e35b5816928.jpeg" alt="Product 3" class="product-image 300px" width="350px">
-                                <div>
-                                    <strong>Cepillos dentales</strong><br>
-                                    Descripción<br>
-                                    Remover
-                                </div>
-                            </div>
-                        </td>
-                        <td>
-                            <div class="quantity-controls">
-                                <button>-</button>
-                                <input type="text" value="1">
-                                <button>+</button>
-                            </div>
-                        </td>
-                        <td>$8.50</td>
-                    </tr>
-                </tbody>
-            </table>
+<div style="background-color: #606c38; padding: 20px 20px; display: flex; justify-content: space-between; align-items: center; color: white;">
+    <strong class="ml-[150px] text-2xl m-8">Carrito de compras</strong>
+
+    <hr class="my-5">
+    </div>
+    <table class="cart-table">
+        <thead>
+            <tr>
+                <th style="color: #283618">Detalles de productos</th>
+                <th style="color: #283618">Cantidad</th>
+                <th style="color: #283618">Precio</th>
+            </tr>
+        </thead>
+        <tr>
+            <td>
+                <div style="margin: 20px; border: 2px solid #BC6C25; border-radius: 8px; overflow: hidden; width: 200px; text-align: center;"
+                    class="product-details">
+                    <img src="https://www.sephora.com/productimages/sku/s1754548-av-8-zoom.jpg?imwidth=315"
+                        alt="Coco Rose" class="product-image height=">
+                    <div>
+                        <strong>Coco Rose</strong><br>
+                        Exfoliante<br>
+                        Remover
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="quantity-controls">
+                    <button>-</button>
+                    <input type="text" value="1">
+                    <button>+</button>
+                </div>
+            </td>
+            <td>$8.50</td>
+        </tr>
+        <tr>
+            <td>
+                <div style="margin: 20px; border: 2px solid #BC6C25; border-radius: 8px; overflow: hidden; width: 200px; text-align: center;"
+                    class="product-details" class="product-details">
+                    <img src="https://www.idun-nature.com/images/products/zao-make-up-paleta-sombras-clin-l-oeil-n2.jpg"
+                        alt="Product 2" class="product-image 300px" width="350px">
+                    <div>
+                        <strong>Maquillaje</strong><br>
+                        Descripción<br>
+                        Remover
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="quantity-controls">
+                    <button>-</button>
+                    <input type="text" value="1">
+                    <button>+</button>
+                </div>
+            </td>
+            <td>$8.50</td>
+        </tr>
+        <tr>
+            <td>
+                <div style="margin: 20px; border: 2px solid #BC6C25; border-radius: 8px; overflow: hidden; width: 200px; text-align: center;"
+                    class="product-details" class="product-details">
+                    <img src="https://d22fxaf9t8d39k.cloudfront.net/47bcf53c18bcfd72061bcf3322313e118738ae0750b0bd24490e6f4283e35b5816928.jpeg"
+                        alt="Product 3" class="product-image 300px" width="350px">
+                    <div>
+                        <strong>Cepillos dentales</strong><br>
+                        Descripción<br>
+                        Remover
+                    </div>
+                </div>
+            </td>
+            <td>
+                <div class="quantity-controls">
+                    <button>-</button>
+                    <input type="text" value="1">
+                    <button>+</button>
+                </div>
+            </td>
+            <td>$8.50</td>
+        </tr>
+        </tbody>
+    </table>
+    </div>
+    <footer style="background-color: #606c38">
+        <div class="w-full mx-auto max-w-screen-xl p-6 md:flex md:items-center md:justify-between">
+            <span style="color: #FEFAE0">© 2024 <a href="https://flowbite.com/" class="hover:underline">ECOMMUNITY</a>.
+                TODOS LOS DERECHOS RESERVADOS
+            </span>
+            <ul style="color: #FEFAE0" class="flex flex-wrap items-center mt-3 text-sm font-medium;">
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">SOBRE NOSOTROS</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">POLITÍCA</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline me-4 md:me-6">ECOMMUNITY</a>
+                </li>
+                <li>
+                    <a href="#" class="hover:underline">CONTACTO</a>
+                </li>
+            </ul>
         </div>
+    </footer>
+
+    </div>
     <section>
 </x-layout>
