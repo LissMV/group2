8d487fea3bc6499fed4e24ed6a03802e32c5ba80
Richarlisson-80 <153464577+Richarlisson-80@users.noreply.github.com>
2024-07-21 01:50:30 -0600

    welcome almost complete

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,110 @@
 <x-layout>
-    <div style="background-color: #606c38; padding: 20px 20px; display: flex; justify-content: space-between; align-items: center; color: white;">
+    <div style="background-color: ">
+        <div
+            style="background-color: #606c38; padding: 20px 20px; display: flex; justify-content: space-between; align-items: center; color: white;">
             <input
-                class=" bg-white w-3/5 h-10 border-double border-neutral-950 border rounded-xl placeholder-black text-1.7xl font-serif"
+                class=" bg-white w-3/5 h-10 border-double border-neutral-950 border rounded-xl placeholder-black text-1.7xl font-serif ml-[230px]"
                 type="text" placeholder="Buscar">
 
             <button type="submit"><img height="25px" width="25px"
                     src="https://cdn-icons-png.flaticon.com/256/25/25313.png">
-                </button>
-    </div>
-    <div style="display: flex; flex-direction: column; align-items: center">
-        <img src="https://www.ecowatch.com/wp-content/uploads/2021/09/176196750-img.jpg"  style="width: 500px">
-
-<section>
-    <font 1>hola</font>
-</section>
-
-        <section class="grid grid-cols-3 content-center">
-            <img ml=10px width=300px src="https://inlovemag.es/wp-content/uploads/2020/02/recetas-de-limpieza-caseras-800x500.jpg" alt="">
-
-            <img width=300px src="https://blog.homedepot.com.mx/wp-content/uploads/2021/01/Co%CC%81mo-germinar-semillas-en-macetas-biodegradables.jpg" alt="">
-
-            <img width=300px src="https://integrishealth.org/-/media/blog/19-jan-feb/jars-with-skincare-products.ashx?revision=007004b7-92c9-4ffa-b02f-47f85cf25894" alt="">
-
-            <img width=300px src="https://m.media-amazon.com/images/I/71MOlm7WtDL._AC_UF350,350_QL80_.jpg" alt="">
-
-            <img width=300px src="https://ae01.alicdn.com/kf/S4c6d6e6c7a414c8aad3b1c193372358bH.jpg?width=1000&height=1000&hash=2000" alt="">
-
-            <img width=300px src="https://dcdn.mitiendanube.com/stores/001/942/058/products/dientes1-a7baf962af00febdca16734606296216-240-0.png" alt="">
-        </section>
-        <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
-            <button style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: white; background-color: #606c38;">Registrarse</button>
-            <button style="width: 150px;  margin: 10px 0;  cursor: pointer; color: black; background-color: white;">Iniciar Sesión</button>
+            </button>
         </div>
+        <div style="display: flex; align-items:center" class="h-15">
+            <img class="mx-[150px]"  src="https://blog.oxfamintermon.org/wp-content/uploads/2016/06/bio.jpg"
+                style="width: 600px">
+                <div style="color: #283618" class="text-1.8xl">
+                <h1>BIODEGRADABLE</h1>
+            <p class="mr-[150px]">La biodegradabilidad es la capacidad de un material de ser biodegradado. Es un proceso natural en el que un material por acción biológica, cambia y en general pierde sus propiedades originales y a nivel químico las moléculas que lo conforma se convierten en formas más simples y estables.</p>
+                    <button style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: #FEFAE0; background-color: #606c38;">Explorar más</button>
+        </div>
+            </div>
+
+            <section style="color: #283618">
+                <p class="ml-[150px] text-2xl m-8">Categorías de productos:</p>
+            </section>
+<section>
+    <style>
+
+        .gallery {
+            display: flex;
+            justify-content: center;
+        }
+        .gallery-item {
+            margin: 20px;
+            border: 2px solid #BC6C25;
+            border-radius: 8px;
+            overflow: hidden;
+            width: 200px;
+            text-align: center;
+        }
+        .gallery-item img {
+            width: 120%;
+            height: 250px;
+            object-fit: cover;
+
+        }
+        .gallery-item:hover img {
+            transform: scale(1.1);
+        }
+        .gallery-item h3 {
+            margin: 10px 0;
+            font-size: 18px;
+            color: #283618;
+        }
+        .gallery-item p {
+            margin: 0 10px 10px;
+            font-size: 15px;
+            color: #606c38;
+        }
+        .gallery-item button {
+            margin-bottom: 10px;
+            padding: 10px 20px;
+            border-radius: 5px;
+            background-color: #BC6C25;
+            color: #FEFAE0;
+            transition: background-color 0.2s;
+        }
+    </style>
+    <div class="gallery">
+        <div class="gallery-item">
+            <img src="https://inlovemag.es/wp-content/uploads/2020/02/recetas-de-limpieza-caseras-800x500.jpg" alt="">
+            <h3>LIMPIEZA</h3>
+            <p></p>
+            <button>Leer Más</button>
+        </div>
+        <div class="gallery-item">
+            <img src="https://www.vivirbiencolmedica.com/wp-content/uploads/2023/08/beneficios-alimentos-naturales.jpg" alt="">
+            <h3>ALIMENTOS</h3>
+            <p></p>
+            <button>Leer Más</button>
+        </div>
+        <div class="gallery-item">
+            <img src="https://media.post.rvohealth.io/wp-content/uploads/2021/03/mask-skin-exfoliate-salt-oil-beauty-732x549-thumbnail-732x549.jpg" alt="">
+            <h3>CUIDADO DE LA PIEL</h3>
+            <p></p>
+            <button>Leer Más</button>
+        </div>
+        <div class="gallery-item">
+            <img src="https://media.puntal.com.ar/p/0192f0d7deac003c8a14871a71dd77d0/adjuntos/279/imagenes/001/196/0001196573/higiene-personal-imagen-pinterest2jpg.jpg" alt="">
+            <h3>HIGIENE PERSONAL</h3>
+            <p></p>
+            <button>Leer Más</button>
+        </div>
+        <div class="gallery-item">
+            <img src="https://down-cl.img.susercontent.com/file/c2d78fc0a67986d2b13455290a72dd51" alt="">
+            <h3>MATERIALES ORGÁNICOS</h3>
+            <p></p>
+            <button>Leer Más</button>
+        </div>
+    </section>
+    <div style="display: flex; flex-direction: column; align-items: center; margin-top: 20px;">
+        <button
+            style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: #FEFAE0; background-color: #606c38;">Registrarse</button>
+        <button
+            style="width: 150px; padding: 10px; margin: 10px 0;  cursor: pointer; color: #283618; background-color: white;">Iniciar
+            Sesión</button>
     </div>
+    </div>
+</section>
 </x-layout>
-
-
