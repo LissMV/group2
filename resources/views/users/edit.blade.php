af9faaf50aeb9d27b3da513988f7f5ae9c9b1ed8
LissMV <valencialisseth47@gmail.com>
2024-07-21 22:16:41 -0600

    intentos backend

--- resources/views/users/edit.blade.php
+++ resources/views/users/edit.blade.php
@@ -1,19 +1,20 @@
 <x-layout>
-    <section class="flex justify-center m-9">
+    <section class="flex justify-center ml-9 mt-24 border-2 border-double border-brown mr-24">
     <div>
         <img class="h-72 w-72" src="{{ asset('img/forms/perfil.png') }}">
     </div>
+
     <div>
     <h1 class="font-bold text-6xl text-center m-8">Editar perfil</h1>
-    <form class="flex justify-center items-center m-8" action="/users/update/{{$user->id}}" method="POST">
+    <form class="flex justify-center items-center" action="/users/update/{{$user->id}}" method="POST">
         @csrf
         @method('PATCH')
         <div class="w-96 p-6">
             <div>
-                <x-input name="name" type="text" value="{{$user->name}}">Name</x-input>
+                <x-input name="name" type="text" value="{{$user->name}}">Nombre</x-input>
             </div>
             <div>
-                <x-input name="email" type="text" value="{{$user->email}}" >Email</x-input>
+                <x-input name="email" type="text" value="{{$user->email}}" >Correo</x-input>
             </div>
 
             <x-button>Actualizar</x-button>
@@ -21,9 +22,12 @@
     </form>
     </div>
     </section>
+
+    <div class="p-2 bg-brown mr-24 ml-9">
     <a href="{{ route('store') }}">
-        <h1>¿Quieres iniciar como vendedor?</h1>
+        <h1 class="text-center font-montserrat text-white2">¿Quieres iniciar como vendedor?</h1>
     </a>
+    </div>
 
     @if($user->is_seller)
     <section class="text-center mb-8">
@@ -36,7 +40,7 @@
 
     @endif
 
-    <h1 class="text-center font-black text-5xl pb-10">Actualizar datos de tienda</h1>
+    <h1 class="text-center text-5xl pb-10 mt-5 font-lora">Actualizar datos de tienda</h1>
 <section class="flex justify-center ">
     <div class="h-72 w-72 pr-10">
         <img class="mt-24" src="{{ asset('img/forms/logoo.png') }}" alt="">
