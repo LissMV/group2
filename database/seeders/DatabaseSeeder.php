29885b847f4270070a64a2833a05cdf43594c9ef
Echeverria7 <153480776+Echeverria7@users.noreply.github.com>
2024-08-14 20:44:33 -0600

    user and store seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,10 +13,62 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        User::factory()->create([
+            'name' => "Seller Ecommunity",
+            'email' => "seller@example1.com",
+            'password' => "password10",
+            'image' => "user 1.png",
+            'is_seller' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => "EcoTrade",
+            'email' => "seller@expample2.com",
+            'password' => "password10",
+            'image' => "user 2.png",
+            'is_seller' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => "BioMarket",
+            'email' => "seller@example3.com",
+            'password' => "password10",
+            'image' => "user 3.png",
+            'is_seller' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => "GreenWave",
+            'email' => "seller@exapmle4.com",
+            'password' => "password10",
+            'image' => "user 4.png",
+            'is_seller' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => "EcoConnect",
+            'email' => "seller@example5.com",
+            'password' => "password10",
+            'image' => "user 5.png",
+            'is_seller' => true,
+        ]);
+
+        User::factory()->create([
+            'name' => "BioBazar",
+            'email' => "seller@example6.com",
+            'password' => "password10",
+            'image' => "user 6.png",
+            'is_seller' => false,
+        ]);
+
+
+
+
+
 
         $this->call([
            CategoriesSeeder::class,
+           StoresSeeder::class,
         ]);
 
       /*  User::factory()->create([
